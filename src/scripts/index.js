5ef7997e7b00ff38046e77a244425e32599e04cf
John Nguyen <nguyen.johnsan@gmail.com>
2019-06-06 07:11:34 -0700

    add basic session and break time functions

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,3 +6,46 @@ import "@fortawesome/fontawesome-free/scss/solid.scss"
 import "@fortawesome/fontawesome-free/scss/fontawesome.scss"
 import '../styles/styles.scss';
 
+// SessionTime Controls
+const sessionDown = document.querySelector("#session-controls i.fa-chevron-circle-down");
+const sessionUp = document.querySelector("#session-controls i.fa-chevron-circle-up");
+const sessionTimeHolder = document.querySelector("#session-time-holder");
+let sessionTime = 25;
+
+function incrementSessionTime() {
+  sessionTime++;
+  sessionTimeHolder.innerText = sessionTime;
+  console.log(`sessionTime Incremented: ${sessionTime}`);
+}
+
+function decrementSessionTime() {
+  sessionTime--;
+  sessionTimeHolder.innerText = sessionTime;
+  console.log(`sessionTime Decremented: ${sessionTime}`);
+}
+
+sessionUp.addEventListener("click", incrementSessionTime);
+
+sessionDown.addEventListener("click", decrementSessionTime);
+
+// BreakTime Control
+const breakDown = document.querySelector("#break-controls i.fa-chevron-circle-down");
+const breakUp = document.querySelector("#break-controls i.fa-chevron-circle-up");
+const breakTimeHolder = document.querySelector("#break-time-holder");
+let breakTime = 5;
+
+function incrementBreakTime() {
+  breakTime++;
+  breakTimeHolder.innerText = breakTime;
+  console.log(`breakTime Incremented: ${breakTime}`);
+}
+
+function decrementBreakTime() {
+  breakTime--;
+  breakTimeHolder.innerText = breakTime;
+  console.log(`breakTime Decremented: ${breakTime}`);
+}
+
+breakUp.addEventListener("click", incrementBreakTime);
+
+breakDown.addEventListener("click", decrementBreakTime);
