55b00f445b8e735e94b3b0e53a7bebc918d58a50
John Nguyen <nguyen.johnsan@gmail.com>
2019-06-07 05:43:46 -0700

    add reset and update inc/dec functions

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,33 +6,43 @@ import "@fortawesome/fontawesome-free/scss/solid.scss"
 import "@fortawesome/fontawesome-free/scss/fontawesome.scss"
 import '../styles/styles.scss';
 
+// Globals
+let sessionTime;
+let breakTime;
+
 // SessionTime Controls
 const sessionDown = document.querySelector("#session-controls i.fa-chevron-circle-down");
 const sessionUp = document.querySelector("#session-controls i.fa-chevron-circle-up");
 const sessionTimeHolder = document.querySelector("#session-time-holder");
-let sessionTime = 25;
+
+function updateSessionTimeText() {
+  sessionTimeHolder.innerText = sessionTime;
+  playerTimeHolder.innerText = addPadding(sessionTime);
+}
 
 function incrementSessionTime() {
   sessionTime++;
-  sessionTimeHolder.innerText = sessionTime;
+  updateSessionTimeText();
   console.log(`sessionTime Incremented: ${sessionTime}`);
 }
 
 function decrementSessionTime() {
-  sessionTime--;
-  sessionTimeHolder.innerText = sessionTime;
-  console.log(`sessionTime Decremented: ${sessionTime}`);
+  if (sessionTime > 0) {
+    sessionTime--;
+    updateSessionTimeText();
+    console.log(`sessionTime Decremented: ${sessionTime}`);
+  }
 }
 
 sessionUp.addEventListener("click", incrementSessionTime);
 
 sessionDown.addEventListener("click", decrementSessionTime);
 
-// BreakTime Control
+// BreakTime Controls
 const breakDown = document.querySelector("#break-controls i.fa-chevron-circle-down");
 const breakUp = document.querySelector("#break-controls i.fa-chevron-circle-up");
 const breakTimeHolder = document.querySelector("#break-time-holder");
-let breakTime = 5;
+
 
 function incrementBreakTime() {
   breakTime++;
@@ -41,11 +51,36 @@ function incrementBreakTime() {
 }
 
 function decrementBreakTime() {
-  breakTime--;
-  breakTimeHolder.innerText = breakTime;
-  console.log(`breakTime Decremented: ${breakTime}`);
+  if (breakTime > 0) {
+    breakTime--;
+    breakTimeHolder.innerText = breakTime;
+    console.log(`breakTime Decremented: ${breakTime}`);
+  }
 }
 
 breakUp.addEventListener("click", incrementBreakTime);
 
 breakDown.addEventListener("click", decrementBreakTime);
+
+// Player Controls
+const playerTimeHolder = document.querySelector("#player-time-holder");
+const resetBtn = document.querySelector("#resetBtn");
+
+function resetPlayer() {
+  sessionTime = 25;
+  breakTime = 5;
+
+  updateSessionTimeText();
+  breakTimeHolder.innerText = breakTime;
+  console.log("Player reset");
+}
+
+resetBtn.addEventListener("click", resetPlayer);
+
+function addPadding(num) {
+  const stringNum = num.toString();
+  return stringNum + ":00";
+}
+
+// initialize
+resetPlayer();
